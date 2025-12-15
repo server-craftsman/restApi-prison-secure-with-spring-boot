e68c7d90bd1bc422238815e11626450c3d7070e9
Nguyễn Đan Huy <huyit2003@gmail.com>
2025-12-15 22:52:20 +0700

    feat: Initialize new Spring Boot project with Maven Wrapper.

---
+++ src/main/java/vn/gov/prison/prisonsecure/prison_secure/PrisonSecureApplication.java
@@ -0,0 +1,13 @@
+package vn.gov.prison.prisonsecure.prison_secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisonSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrisonSecureApplication.class, args);
+	}
+
+}
